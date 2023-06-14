2d3d029aa5e7b3b3ac11e3b493baec3d9968934a
RADEN SEHAN <radensyaikhan@gmail.com>
2023-06-14 13:24:55 +0700

    add filter and bar chart

--- app.py
+++ app.py
@@ -3,16 +3,32 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
-DATA_URL = (
+tab1, tab2 = st.tabs(["🗒️ About","📈 Chart"])
+df = pd.read_excel(
     "Esport data.xlsx"
 )
 
-st.title("Esports Earnings")
-st.markdown("Esports (olahraga elektronik) adalah bentuk kompetisi olahraga yang menggunakan video game. Esports sering kali berbentuk kompetisi video game multipemain yang terorganisir, terutama antara pemain profesional, baik secara individu maupun tim.")
+#tab1
+tab1.title("Esports Earnings")
 image = Image.open('esport.png')
-st.image(image, use_column_width = True)
+tab1.image(image, use_column_width = True)
+tab1.markdown("Esports (olahraga elektronik) adalah bentuk kompetisi olahraga yang menggunakan video game. Esports sering kali berbentuk kompetisi video game multipemain yang terorganisir, terutama antara pemain profesional, baik secara individu maupun tim.")
 
 #sidebar
-st.sidebar.markdown("## Side Panel")
-st.sidebar.markdown("Use this panel to explore the dataset and create own viz.")
+st.sidebar.markdown("## Filter Column")
 
+#tab2
+tab2.title("Data Earnings Esports")
+genre_filter = st.sidebar.selectbox("Filter by Genre", options=df['Genre'].unique())
+view_all = st.sidebar.checkbox("View all data")
+if view_all:
+    tab2.dataframe(df)
+
+filter_data = df[
+    (df['Genre'] == genre_filter)
+]
+if not view_all:
+    tab2.dataframe(filter_data)
+
+tab2.write("Bar Chart:")
+tab2.bar_chart(filter_data, x="Game", y="TotalPlayers")
